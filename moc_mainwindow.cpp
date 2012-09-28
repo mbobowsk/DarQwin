e562207519e081a47c4efc85302a62733b195ccf
Michał Bobowski <m_bobowski@o2.pl>
2012-09-28 12:59:28 +0200

    Pierwsze próby rysowania histogramu.

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Thu Sep 27 20:04:19 2012
+** Created: Fri Sep 28 12:59:11 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
