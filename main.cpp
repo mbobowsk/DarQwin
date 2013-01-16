1905d13e44ff4ad870131c02ec16e4527ffe696a
Michał Bobowski <m_bobowski@o2.pl>
2013-01-16 16:01:20 +0100

    Sterowniki do JPEG i TIFF.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //dodaj sterownik do jpeg
+    QString dir = QCoreApplication::applicationDirPath();
+    dir.append("/plugins/imageformats");
+    a.addLibraryPath(dir);
+
     MainWindow w;
     w.show();
     return a.exec();
